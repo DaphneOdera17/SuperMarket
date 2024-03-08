d40d9a0b6ff5c692954cb3363cdcb56ea66eabda
DaphneOdera17 <2944006086@qq.com>
2024-03-08 23:27:45 +0800

    update

---
+++ include/Admin/Admin_Login.h
@@ -0,0 +1,8 @@
+#ifndef _ADMIN_LOGIN_H_
+#define _ADMIN_LOGIN_H_
+
+void Admin_Login();
+
+#endif
+
+
--- src/Admin/Admin_Login.c
+++ src/Admin/Admin_Login.c
@@ -1,29 +1,74 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "Menu/Main_Menu.h"
 
 void Admin_Login()
 {
-
+    char ch;
     char name[20];
-    char password[20] = '\0';
+    char password[20];
+    char save_name[20];
+    char save_password[20];
 
     printf("请输入管理员姓名:  ");
-    scanf("%s",&name);
+    scanf("%s", name);
     getchar();
 
     int i = 0;
     printf("请输入密码:  ");
-    while (i < 20)
+    while(1)
     {
         system("stty -echo");
-        password[i] = getchar();
+        ch = getchar();
         system("stty -echo");
-        if(password[i++] == '\n')
+        
+        if(ch == '\n')
         {
+            password[i] = '\0';
             break;
-            printf("*");
+        }
+        else
+        {
+            password[i++] = ch;
         }
     }
+
+    FILE *ptr;
+    ptr = fopen("Admin.txt", "r");
+    if(ptr == NULL)
+    {
+        printf("error");
+    }
+
+    char line[256];
+    int user_found = 0;
+    while(fgets(line ,sizeof(line) , ptr) != NULL)
+    {
+        if(sscanf(line , "%s %s" , save_name , save_password) == 2)
+        {
+            if(strcmp(name,save_name) == 0)
+            {
+                user_found = 1;
+                int k = strcmp(password, save_password);
+                if(k = 0)
+                {
+                    printf("登陆成功!");
+                    // Admin_Menu();
+                    fclose(ptr);
+                    
+                }
+                else
+                {
+                    printf("登陆失败，请重新再试！");
+                    Main_Menu();
+                    // 重新输入用户名功能？
+                }
+            }
+        }
+        
+    }
+    
     
     
     
